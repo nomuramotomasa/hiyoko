e61f3acf101ad0399b54e43ef31998914ea0bf02
YushiKoyama <ys.koyama@core-tech.jp>
2023-05-30 17:58:00 +0900

    他人のプロフ

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HiyokoUserController;
 use App\Http\Controllers\TweetController;
+use App\Http\Controllers\Others\OtherProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{user_id}', [OtherProfileController::class, 'index']);
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
