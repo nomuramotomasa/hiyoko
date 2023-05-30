d51d94e098436154503177877d835d54398f7863
nomuramotomasa <m.nomura@core-tech.jp>
2023-05-30 10:44:34 +0900

    マイグレーションとモデル

---
+++ app/Models/hiyoko_user.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class hiyoko_user extends Model
+{
+    use HasFactory;
+}
