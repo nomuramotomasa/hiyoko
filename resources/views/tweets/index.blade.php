0eac46d2e306e6327bf74de515f3be7c4dc99cae
KimiwadaSuzune <s.kimiwada@core-tech.jp>
2023-05-31 09:39:54 +0900

    昨日の分です

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -9,23 +9,23 @@
 </head>
 <body>
     <header>
-        <h1><a href="#"><img src="..\images\logo.png" alt="ぴよったー"></a></h1>
-        {{-- <h1 class="title">ぴよったー</h1> --}}
+        <h1><a href="{{ route('tweets.index') }}"><img src="..\images\logo.png" alt="ぴよったー"></a></h1>
     </header>
     <article>
         <div class="side">
             <p>MENU</p>
             <ul>
-
-                {{-- <li><a href="">プロフィール</a></li>
-                <li><a href="">ツイート一覧</a></li>
-                <li>ログアウト</li> --}}
+                <li><a href="{{ route('tweets.index') }}">プロフィール</a></li>
+                <li><a href="{{ route('tweets.show') }}">ツイート一覧</a></li>
+                <li><a href="{{ route('tweets.index') }}">ログアウト</a></li>
         </div>
         <div class="content">
           <p>タイムライン</p>
-
+            {{-- タイムラインを表示させる --}}
         </div>
     </article>
-
+    <footer>
+        <p>© 2023 株式会社コアテック. All rights reserved.</p>
+    </footer>
 </body>
 </html>
