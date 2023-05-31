474e61bb37921fd1bdca87d1380f93015f9ca774
nomuramotomasa <m.nomura@core-tech.jp>
2023-05-31 15:37:06 +0900

    ユーザー一覧　end

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,21 +1,24 @@
+<body background=../images/background.png>
 <div style="text-align: center;">
     <img src="..\images\logo.png" alt="ぴよったー">
     <h1>UserID検索</h1>
 </div>
-<form action="{{ route('search.search') }}" method="get">
+<div style="text-align: center;">
+  <form action="{{ route('search.search') }}" method="get">
 
-  <div class="form-group",>
-    <label>UserID</label>
-    <input type="text" class="form-control col-md-5" placeholder="検索したいUserIDを入力してください" name="name">
+    <div class="form-group",>
+        <label>UserID</label>
+        <input type="text" class="form-control col-md-5" placeholder="検索したいUserIDを入力してください" name="name">
+    </div>
+    <button type="submit" class="btn btn-primary col-md-5">検索</button>
   </div>
+  </form>
+</div>
 
-  <button type="submit" class="btn btn-primary col-md-5">検索</button>
-</form>
 
-<body background=../images/background.png>
-    <div style="margin-top:50px;">
+    <div style=" text-align: center;">
         <h1>ユーザー一覧</h1>
-        <table class="table">
+        <table class="table" style="margin: auto;">
         <tr>
             <th>ユーザーID</th>
         </tr>
